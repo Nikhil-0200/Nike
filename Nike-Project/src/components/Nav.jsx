24944822bb9100d7159d48c4c98c2c078825f1ca
Nikhil-0200 <nikhiltiwari0200@gmail.com>
2024-01-27 14:49:31 +0530

    Nav Bar Made

---
+++ Nike-Project/src/components/Nav.jsx
@@ -0,0 +1,40 @@
+import {headerLogo} from '../assets/images'
+import { navLinks } from '../constants'
+import { hamburger } from '../assets/icons'
+
+const Nav = () => {
+  return (
+    <header className="padding-x py-8 absolute z-10 w-full">
+      <nav className='flex justify-between items-center max-container'>
+        <a href="/">
+          <img 
+          src={headerLogo} 
+          alt="Logo"
+           width={130}
+           height={29}
+          />
+        </a>
+        <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
+          {navLinks.map((items)=>(
+            <li key={items.label}>
+              <a 
+              href={items.href} className='font-montserrat leading-normal text-lg text-slate-gray'>
+                {items.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div className="hidden max-lg:block">
+          <img 
+          src={hamburger} 
+          alt="Menu Logo"
+          width={25}
+          height={25}
+          />
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+export default Nav
---
+++ Nike-Project/src/sections/index.js
@@ -0,0 +1,20 @@
+import Hero from "./Hero";
+import CustomerReviews from "./customerReviews";
+import Footer from "./footer";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+
+
+export {
+    Hero, 
+    CustomerReviews, 
+    Footer, 
+    PopularProducts, 
+    Services, 
+    SpecialOffer, 
+    Subscribe, 
+    SuperQuality
+}
